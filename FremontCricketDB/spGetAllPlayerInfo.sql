2a49a10a7c98d9d78c62e3b146e1031395b6b7c1
GeethaKh <geetha.fullstackdev@gmail.com>
2025-03-02 17:59:30 -0800

    DB project added

---
+++ FremontCricketDB/spGetAllPlayerInfo.sql
@@ -0,0 +1,25 @@
+﻿USE [FremontCricket]
+GO
+/****** Object:  StoredProcedure [dbo].[spGetAllPlayerInfo]    Script Date: 3/2/2025 5:51:38 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[spGetAllPlayerInfo] 
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT 
+	ROW_NUMBER() OVER(ORDER BY ui.first_name),
+	ui.first_name, 
+	ui.last_name, 
+	0,
+	0,
+	0,
+	0
+	from  user_info ui 
+END
